7b603ddce085476700e89443a961dc94e9b254df
abhinav375 <ADM-IG-HWDLAB1C@PUNHWD13102.corp.capgemini.com>
2018-08-18 12:51:57 +0530

    added add discount functionality

--- src/main/java/com/capgemini/storeserver/services/AdminServices.java
+++ src/main/java/com/capgemini/storeserver/services/AdminServices.java
@@ -1,10 +1,12 @@
 package com.capgemini.storeserver.services;
 
+import com.capgemini.storeserver.beans.Discount;
 import com.capgemini.storeserver.beans.Merchant;
 
 
 public interface AdminServices {
 	
 	public Merchant addMerchant(Merchant merchant);
+	public Discount addDiscount(Discount discount);
 	
 }
