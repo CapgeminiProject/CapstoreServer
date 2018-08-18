32d405258c6eeda033255833c3ee927fed5b7824
Akshat Chawla <akshatchawla02@gmail.com>
2018-08-18 12:43:51 +0530

    View All merchant functionality added.

--- src/main/java/com/capgemini/storeserver/services/AdminServices.java
+++ src/main/java/com/capgemini/storeserver/services/AdminServices.java
@@ -1,10 +1,14 @@
 package com.capgemini.storeserver.services;
 
+import java.util.List;
+
 import com.capgemini.storeserver.beans.Merchant;
 
 
 public interface AdminServices {
 	
 	public Merchant addMerchant(Merchant merchant);
+	
+	public List<Merchant> viewAllMerchants();
 	
 }
